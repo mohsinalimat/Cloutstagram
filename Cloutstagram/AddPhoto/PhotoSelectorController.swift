df4899e991c57879b1a590ac6484d6ddbf553900
Ali Sanaknaki <asana085@uottawa.ca>
2017-10-05 08:33:28 -0400

    Photo selector for upload is somewhat a thing now

--- Cloutstagram/AddPhoto/PhotoSelectorController.swift
+++ Cloutstagram/AddPhoto/PhotoSelectorController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    // ID's
+    let cellId = "cellId"
+    let headerId = "headerId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +21,89 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
         
         setupNavigationButtons()
         
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
+        
+        // Chosen picture header
+        collectionView?.register(PhotoSelectorHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        
+        fetchPhotos()
+    }
+    
+    var selectedImage: UIImage?
+    
+    // Select a photo from the grid
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Selected image
+        self.selectedImage = images[indexPath.item]
+        
+        self.collectionView?.reloadData()
+    }
+    
+    // Array of images
+    var images = [UIImage]()
+    
+    // Fetch photos to load
+    fileprivate func fetchPhotos(){
+        
+        // How we going to fetch pictures
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.fetchLimit = 15
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+        fetchOptions.sortDescriptors = [sortDescriptor] // Display most recent pictures in the grid first
+        
+        // Fetch all photos from lib
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        
+        // A for loop for all fetched photos
+        allPhotos.enumerateObjects({ (asset, count, stop) in
+            
+            // Sizing out the picture to properly fit
+            let imageManager = PHImageManager.default()
+            let targetSize = CGSize(width: 350, height: 350)
+            let options = PHImageRequestOptions()
+            options.isSynchronous = true
+            imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+                
+                // Push image to image array
+                if let image = image {
+                    self.images.append(image)
+                    
+                    if self.selectedImage == nil {
+                        self.selectedImage = image
+                    }
+                }
+                
+                // Call when you done fetching all photos
+                if count == allPhotos.count - 1 {
+                    self.collectionView?.reloadData()
+                }
+            })
+            
+        })
+    }
+    
+    // Allow a line underneath the chosen header picture
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    
+    // Chosen picture size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        let width = view.frame.width
+        
+        return CGSize(width: width, height: width)
+    }
+    
+    // Override header function
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! PhotoSelectorHeader
+        
+        // Turn header to chosen picture
+        header.photoImageView.image = selectedImage
+        
+        return header
     }
     
     // Size of cells
@@ -39,14 +126,14 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     
     // NumOf
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return images.count
     }
     
     // Return the cells
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PhotoSelectorCell
         
-        cell.backgroundColor = .blue
+        cell.photoImageView.image = images[indexPath.item]
         
         return cell
     }
---
+++ Cloutstagram/AddPhoto/PhotoSelectorHeader.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoSelectorHeader.swift
+//  Cloutstagram
+//
+//  Created by sana on 2017-10-04.
+//  Copyright © 2017 sanaknaki. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell {
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill // Resize picture to fit grids to fit smaller sizes
+        iv.clipsToBounds = true // Stay in bounds of a cell
+        iv.backgroundColor = .cyan
+        
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        backgroundColor = .brown
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not beem implemented")
+    }
+}
